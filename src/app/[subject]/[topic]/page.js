f9cd9c26783e278bef1be4205a180c0eba2276d0
rashmitank27 <rashmitank27@gmail.com>
2024-05-02 15:08:14 -0700

    Styles updated

--- src/app/[subject]/[topic]/page.js
+++ src/app/[subject]/[topic]/page.js
@@ -19,22 +19,6 @@ export default async function TutorialPage({ params }) {
     let subjectDetails;
     let topicContent;
 
-    // if(!subject && !topic) {
-    //     console.log("undefined");
-    //     firestoreData.map(data => {
-    //         if(data.id === "flutter"){
-    //             subjectDetails = data;
-    //             console.log("subjectDetails: ", subjectDetails);
-    //         }
-    //     });
-    //     subjectDetails.content.map(data => {
-    //         if(data.url === "introduction") {
-    //             topicContent = data.content;
-    //             console.log("topicContent: ", topicContent);
-    //         }
-    //     });
-    // }
-
     firestoreData.map(data => {
         if(data.id === subject){
             subjectDetails = data;
@@ -52,7 +36,7 @@ export default async function TutorialPage({ params }) {
     return (
         <Layout subjectDetails = {subjectDetails} firestoreData = {firestoreData}>
             <div className="min-h-screen flex flex-col">
-                <div className="m-8 prose">
+                <div className="m-8 prose max-w-none">
                     <Markdown>{topicContent}</Markdown>
                 </div>
             </div>
@@ -89,40 +73,3 @@ export default async function TutorialPage({ params }) {
         description: desc
       }
   }
-
-// export default async function Tutorial({ params }) {
-//     let subjectName = params.tutorial;
-//     let name = "";
-//     console.log("params in page: ", params);
-//     console.log("params in page: ", params.tutorial);
-//     const data = await getTutorials(params.tutorial);
-//     console.log("data received: ", data);
-//         if (!data) {
-//             console.log("Error: Invalid data received: ", data);
-//             name = "";
-//         } else {
-//             name = data.name;
-//         }
-//         name = data[0].name
-    
-//     let subjectDetails;
-//     data.map(subject => {
-//         if(subject.id === params.tutorial){
-//             console.log("subject: ", subject);
-//             subjectDetails = subject;
-//             console.log("subjectDetails: ", subjectDetails.content);
-//         }
-//     });
-    
-
-//     return (
-//         <Layout pageTitle='title' subjectName = {subjectName} subjectDetails = {subjectDetails}>
-//             <div className="min-h-screen flex flex-col">
-//                 <div className="m-auto">
-//                     <h1 className="text-4xl">title</h1>
-//                     <p>{name}</p>
-//                 </div>
-//             </div>
-//         </Layout>
-//     )
-//   }
