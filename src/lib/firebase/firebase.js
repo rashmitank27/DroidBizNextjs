72f8b0c4df21346292f089f4448873624a0b8911
rashmitank27 <rashmitank27@gmail.com>
2024-05-06 14:00:03 -0700

    env variables removed

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.projectId,
+//     storageBucket: process.env.storageBucket,
+//     messagingSenderId: process.env.messagingSenderId,
+//     appId: process.env.appId
+// };
+
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
+    apiKey: "AIzaSyC4aO3mjR78muVuHSsKauIGx87RsenOKjo",
+    authDomain: "droidbiz-angular.firebaseapp.com",
+    projectId: "droidbiz-angular",
+    storageBucket: "droidbiz-angular.appspot.com",
+    messagingSenderId: "11075113010",
+    appId: "1:11075113010:web:f64dad631325a8b9e1ec2b"
 };
 
 const app = initializeApp(firebaseConfig)
