d8611c3dd85ee106524655a3b161eefd663bdc46
rashmitank27 <rashmitank27@gmail.com>
2024-05-08 11:37:50 -0700

    vercel env var added

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <GoogleAdsenseScript/>    
+      {/* <GoogleAdsenseScript/>     */}
     </html>
   );
 }
