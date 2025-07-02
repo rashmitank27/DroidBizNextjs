8ca16c66b09eb4092c151aec6c48d3cbd8b1c103
rashmitank27 <rashmitank27@gmail.com>
2025-07-02 16:00:52 -0700

    Update GAdsense.js

--- src/components/GAdsense.js
+++ src/components/GAdsense.js
@@ -2,7 +2,7 @@ import Script from 'next/script';
 const GoogleAdsenseScript = () => {
     return (
       <Script
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.googleClientId}`}
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
         strategy="afterInteractive"
         crossOrigin="anonymous"
       />
