b3621fb6e48826ae7612c4511584270b6a2730cd
rashmitank27 <rashmitank27@gmail.com>
2025-07-02 16:30:43 -0700

    Update GAdsense.js

--- src/components/GAdsense.js
+++ src/components/GAdsense.js
@@ -1,12 +1,20 @@
 import Script from 'next/script';
+
 const GoogleAdsenseScript = () => {
-    return (
-      <Script
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
-        strategy="afterInteractive"
-        crossOrigin="anonymous"
-      />
-    );
-  };
-  
-  export default GoogleAdsenseScript;
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+      onLoad={() => {
+        console.log('AdSense script loaded successfully')
+      }}
+      onError={(e) => {
+        console.error('AdSense script failed to load', e)
+      }}
+    />
+  );
+};
+
+export default GoogleAdsenseScript;
